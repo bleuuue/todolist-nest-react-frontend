4350b9cc3b6da5a35c6bb7355ddce29dad51103d
bleuuue <bleuuue@gmail.com>
2021-08-17 10:30:13 +0900

    swr

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import axios from 'axios';
-import { useState } from 'react';
 import AddTodo from './AddTodo';
+import useSWR from 'swr';
 
 interface Itodo {
   id: number;
@@ -13,27 +13,43 @@ interface Itodo {
 }
 
 const Todolist: FC = () => {
-  const [todos, setTodos] = useState<Itodo[]>([]);
+  const fetcher = async (url: string) => {
+    try {
+      const response = await axios.get(url);
 
-  useEffect(() => {
-    const getTodos = async () => {
-      try {
-        const response = await axios.get('http://localhost:3010/todo');
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-        if (response.statusText === 'OK') {
-          setTodos(response.data);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getTodos();
-  }, []);
+  const { data, error } = useSWR<Itodo[]>(
+    `${process.env.REACT_APP_BACK_URL}/todo`,
+    fetcher,
+  );
+
+  if (!data) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+
+  // useEffect(() => {
+  //   const getTodos = async () => {
+  //     try {
+  //       const response = await axios.get('http://localhost:3010/todo');
+
+  //       if (response.statusText === 'OK') {
+  //         setTodos(response.data);
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   getTodos();
+  // }, [data]);
 
   return (
     <div>
       <AddTodo />
-      {todos.map((todo) => {
+      {data.map((todo) => {
         return (
           <li key={todo.id}>
             {todo.id} - {todo.title} - {todo.desc}
